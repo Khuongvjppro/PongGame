5f9ef68eeef1b1e6cf28dc331b282f062f715a96
Khuongvjppro <22130129@st.hcmuaf.edu.vn>
2024-07-07 02:19:49 +0700

    Code Pong Game

---
+++ PongGame.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class PongGame extends JFrame {
+
+	public static void main(String[] args) {
+		GameFrame frame = new GameFrame();
+	}
+}
